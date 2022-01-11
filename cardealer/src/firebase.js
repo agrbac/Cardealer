28ffcbb5edb9a96701ef962fecc3e0516941ba42
unknown <agrbac@unipu.hr>
2022-01-11 17:18:43 +0100

    novo

--- cardealer/src/firebase.js
+++ cardealer/src/firebase.js
@@ -1,17 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { } from 'firebase/auth';
-import { } from 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDrVsJiGeKaGLGVZd9KIQt6jlJDYPcMCE0",
-    authDomain: "cardealer-75aaa.firebaseapp.com",
-    projectId: "cardealer-75aaa",
-    storageBucket: "cardealer-75aaa.appspot.com",
-    messagingSenderId: "394688301658",
-    appId: "1:394688301658:web:5113250f0686668525a663"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  export default firebase;
+  apiKey: "AIzaSyDrVsJiGeKaGLGVZd9KIQt6jlJDYPcMCE0",
+  authDomain: "cardealer-75aaa.firebaseapp.com",
+  projectId: "cardealer-75aaa",
+  storageBucket: "cardealer-75aaa.appspot.com",
+  messagingSenderId: "394688301658",
+  appId: "1:394688301658:web:5113250f0686668525a663"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- cardealer/src/views/Signup.vue
+++ cardealer/src/views/Signup.vue
@@ -7,41 +7,53 @@
         <div class="col-sm">
           <form>
             <div class="form-group">
+              <label for="exampleInputUsername">Username</label>
+              <input
+                type="username"
+                v-model="username"
+                class="form-control"
+                id="exampleInputUsername"
+                placeholder="Enter username"
+              />
+            </div>
+            <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
               <input
                 type="email"
-                v-model="username"
+                v-model="email"
                 class="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
                 placeholder="Enter
 email"
               />
-              <small id="emailHelp" class="form-text text-muted"
-                >We'll never share your email with anyone else.</small
-              >
             </div>
             <div class="form-group">
               <label for="exampleInputPassword1">Password</label>
               <input
                 type="password"
-                 v-model="password"
+                v-model="password"
                 class="form-control"
                 id="exampleInputPassword1"
                 placeholder="Password"
               />
+              <small id="passwordHelp" class="form-text text-muted"
+                >Password should be at least 6 characters</small
+              >
             </div>
             <div class="form-group">
               <label for="exampleInputPassword1">Repeat password</label>
               <input
                 type="password"
-                 v-model="passwordRepeat"
+                v-model="passwordRepeat"
                 class="form-control"
                 id="exampleInputPassword1"
                 placeholder="Password"
               />
             </div>
-            <button type="button" @click="signup" class="btn btn-primary">Submit</button>
+            <button type="button" @click="signup" class="btn btn-primary">
+              Submit
+            </button>
           </form>
         </div>
         <div class="col-sm"></div>
@@ -51,22 +63,41 @@ email"
 </template>
   
   <script>
-  import firebase from "@/firebase";
+import firebase from "@/firebase";
 export default {
-  name:"signup",
+  name: "signup",
   data() {
-    return{
-      username: '',
-      password: '',
-      passwordRepeat: '',
+    return {
+      username: "",
+      password: "",
+      passwordRepeat: "",
     };
-    
   },
-  methods:{
-signup(){
-  console.log(firebase);
-}
-  }
+  methods: {
+    signup() {
+      if (this.password != this.passwordRepeat) {
+        alert("Confirm password not match");
+        return;
+
+        if ((this.username, this.email == null)) {
+          alert("cant be blank");
+          return;
+        }
+      } else {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.email, this.password)
+          .then(function () {
+            console.log("uspjesna registracija");
+          })
+          .catch(function (error) {
+            console.error("greska", error);
+          });
+        console.log("nastavak");
+        alert("uspjesna registracija");
+      }
+    },
+  },
 };
 </script>
 
